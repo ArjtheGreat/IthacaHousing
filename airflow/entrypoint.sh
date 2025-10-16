3af01869c8d3c4af90a2656b64186744a72b1f92
Arj <33406133+ArjtheGreat@users.noreply.github.com>
2025-10-16 01:34:20 -0400

    holy airflow

--- airflow/entrypoint.sh
+++ airflow/entrypoint.sh
@@ -5,25 +5,27 @@ echo "Starting Airflow Setup..."
 
 export AIRFLOW__CORE__SIMPLE_AUTH_MANAGER_ALL_ADMINS=True
 
-if [ ! -f /opt/airflow/airflow_initialized ]; then
-    echo "Resetting Airflow DB..."
-    airflow db reset -y  
+if [ ! -f "${AIRFLOW_HOME}/airflow_initialized" ]; then
+    echo "Initializing Airflow DB..."
+    airflow db reset -y
     airflow db init
 
-    echo "Creating admin user (Airflow 3.0)..."
+    echo "Creating admin user..."
     airflow users create \
         --username admin \
         --firstname Admin \
         --lastname User \
         --role Admin \
-        --email asmaitra2006@gmail.com \
+        --email admin@example.com \
         --password admin
 
-    touch /opt/airflow/airflow_initialized
+    touch "${AIRFLOW_HOME}/airflow_initialized"
 else
-    echo "Airflow DB already initialized. Skipping DB setup."
+    echo "Airflow already initialized. Skipping setup."
 fi
 
+echo "Starting scheduler..."
 airflow scheduler &
 
+echo "Starting webserver..."
 exec airflow webserver
